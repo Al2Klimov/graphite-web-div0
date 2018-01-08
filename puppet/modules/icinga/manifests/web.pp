7d566cc968a194164564ba70b277768c83385c15
Alexander A. Klimov <alexander.klimov@netways.de>
2018-01-08 10:40:56 +0100

    Make Icinga Web 2 actually working

--- puppet/modules/icinga/manifests/web.pp
+++ puppet/modules/icinga/manifests/web.pp
@@ -15,4 +15,19 @@ class icinga::web {
 	package { 'icingaweb2':
 		require => Apt::Repo['packages.icinga.com'],
 	}
+
+	file { '/etc/apache2/mods-enabled/proxy.load':
+		ensure  => link,
+		target  => '/etc/apache2/mods-available/proxy.load',
+		require => Package[['apache2', 'icingaweb2']],
+	}
+	-> file { '/etc/apache2/mods-enabled/proxy_fcgi.load':
+		ensure => link,
+		target => '/etc/apache2/mods-available/proxy_fcgi.load',
+	}
+	-> file { '/etc/apache2/conf-enabled/php7.0-fpm.conf':
+		ensure => link,
+		target => '/etc/apache2/conf-available/php7.0-fpm.conf',
+		notify => Service['apache2'],
+	}
 }
